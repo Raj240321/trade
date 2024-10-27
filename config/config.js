fda63b19ff66d6dd6e9d2e916cb1394b856c9545
yudiz_ranjeet <ranjeet.kumar@yudiz.com>
2024-10-27 12:21:44 +0530

    add hard code mongo Url

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ console.log(process.env.NODE_ENV, 'Server Start...')
 const environment = {
   PORT: process.env.PORT || 1101,
   NODE_ENV: process.env.NODE_ENV || 'local',
-  MONGO_URL: process.env.MONGO_URL || '',
+  MONGO_URL: process.env.MONGO_URL || 'mongodb+srv://poweruniverse444:fJOFXsrWuAJ6l0Cn@cluster0.g5j2n.mongodb.net/trade?retryWrites=true&w=majority&appName=Cluster0',
   REDIS_PORT: process.env.REDIS_PORT || 6379,
   REDIS_HOST: process.env.REDIS_HOST || 'localhost',
   REDIS_PASS: process.env.REDIS_PASS || false,
