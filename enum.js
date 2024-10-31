13557c9b1ab129beae0b9dc2430c9d0c79c22fe8
yudiz_ranjeet <ranjeet.kumar@yudiz.com>
2024-10-31 17:52:49 +0530

    add user role

--- enum.js
+++ enum.js
@@ -2,7 +2,7 @@ const enums = {
   gender: ['male', 'female', 'other', 'nil'],
   status: ['y', 'n', 'd'],
   smsProvider: [],
-  role: ['superMaster', 'master', 'broker'],
+  role: ['superMaster', 'master', 'broker', 'user'],
   transactionReason: ['add balance', 'withdraw balance', 'wrong balance', 'error correction']
 }
 module.exports = enums
